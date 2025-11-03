87a3ff796a8c57d0141a2fd081b206ddfc8039aa
mslmalmyaly223-sudo <mslmalmyaly223@gmail.com>
2025-11-04 02:55:47 +0300

    Add basic service worker with install and fetch events

---
+++ Sw.js
@@ -0,0 +1,8 @@
+// sw.js
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
