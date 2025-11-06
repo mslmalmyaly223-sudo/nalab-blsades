fb67768e2d22e846659a250c9c6262901937410e
mslmalmyaly223-sudo <mslmalmyaly223@gmail.com>
2025-11-06 06:41:45 +0300

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,8 +1,7 @@
-// sw.js
 self.addEventListener('install', function(event) {
-  console.log('Service Worker installed');
+  event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
 });
