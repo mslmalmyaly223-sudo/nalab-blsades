dd28fb5b995b7bf1ff88de4066aee81f9c0e5ff0
mslmalmyaly223-sudo <mslmalmyaly223@gmail.com>
2025-12-09 14:38:53 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+// sw.js - تحديث هذا الملف
+const CACHE_NAME = 'souk-tullab-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/firebase-messaging-sw.js',
+  // أضف بقية الملفات
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
